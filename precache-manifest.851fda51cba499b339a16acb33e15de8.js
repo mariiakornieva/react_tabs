82ec89c3388768357a9d3023c235d9440d249b10
Mariia Kornieva <mariiakornieva@gmail.com>
2021-05-14 11:29:21 +0300

    Updates

---
+++ precache-manifest.851fda51cba499b339a16acb33e15de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f15b7032410f3056c01bb19af9f8980",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3f905d44e9997813bb9",
+    "url": "./static/css/main.afea4022.chunk.css"
+  },
+  {
+    "revision": "5cf68f95d9fe35843b32",
+    "url": "./static/js/2.61b16578.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.61b16578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f905d44e9997813bb9",
+    "url": "./static/js/main.7f7aae32.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
